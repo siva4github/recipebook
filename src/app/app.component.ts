7c8e28771f6152d8e8673c6fa10ec5d53d2cb585
Siva Alamalakala <>
2020-06-19 18:40:38 +0530

    Added Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-book';
+  navigation = 'recipe';
+
+  onNavigation(inputNavigation: string)
+  {
+    this.navigation = inputNavigation;
+  }
 }
